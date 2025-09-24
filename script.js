8231ac795e7b4562d35b3c84816ae429e729750a
Swapnaja27 <142212367+Swapnaja27@users.noreply.github.com>
2025-09-24 11:11:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function cells(){
+	let arr=[];
+	for(let i=0;i<=9;i++){
+		arr.push(document.getElementById(String(i)));
+	}
+	return arr;
+}
+function resetall(){
+	cells().forEach((el)=>
+		el.style.backgroundColor="tansparent";
+		)
+}
+
+document.getElementByID("change_button").addEventListener("click",()=>{
+	const id=document.getElementById("block_id").value.trim();
+  const color = document.getElementById("colour_id").value.trim();
+  resetAll();
+  const cell = document.getElementById(id);
+  if (cell && color) cell.style.backgroundColor = color;
+
+})
+
+document.getElementById("reset_button").addEventListener("click", resetAll);
